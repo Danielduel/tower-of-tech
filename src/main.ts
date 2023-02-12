80f14cfbdcf6294437cdbe4ab23e90e7154a4dfa
Daniel Mizerski <duelsik@gmail.com>
2023-02-12 22:15:44 +0100

    update pool, add cli

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,20 @@
+import $ from "https://deno.land/x/dax@0.26.0/mod.ts";
 
+const options = [
+  "Migrate playlists",
+  "Update and migrate playlists",
+] as const;
+const index = await $.select({
+  message: "What's your favourite colour?",
+  options: options as unknown as string []
+});
 
+switch (options[index]) {
+  case "Migrate playlists": 
+    await $`deno task migratePlaylists`;
+    break;
+  case 'Update and migrate playlists':
+    await $`deno task refreshPlaylists`;
+    await $`deno task migratePlaylists`;
+    break;
+}
