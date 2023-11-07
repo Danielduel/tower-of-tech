ab3b597fb4a8e97ec4c25bb0fc879de1395b7df8
Daniel Mizerski <duelsik@gmail.com>
2023-11-08 00:05:17 +0100

    test discord bot

---
+++ scripts/register-tot-bot-slash-commands.ts
@@ -0,0 +1,27 @@
+import { load } from "https://deno.land/std@0.205.0/dotenv/mod.ts";
+
+const {
+  DISCORD_TOT_BOT_CLIENT_ID,
+  DISCORD_TOT_BOT_TOKEN,
+} = await load();
+
+fetch(
+  `https://discord.com/api/v8/applications/${DISCORD_TOT_BOT_CLIENT_ID}/commands`,
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bot ${DISCORD_TOT_BOT_TOKEN}`,
+    },
+    body: JSON.stringify({
+      name: "hello",
+      description: "Test command / Test if bot is alive",
+      options: [{
+        name: "name",
+        description: "The name of the person",
+        type: 3,
+        required: true,
+      }],
+    }),
+  },
+);
