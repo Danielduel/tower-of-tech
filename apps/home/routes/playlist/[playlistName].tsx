aec76e55c6b24d89c941affdd945471472dde1b4
Daniel Mizerski <duelsik@gmail.com>
2023-11-11 03:17:45 +0100

    test fresh 2

--- apps/home/routes/playlist/[playlistName].tsx
+++ apps/home/routes/playlist/[playlistName].tsx
@@ -2,7 +2,7 @@ import { PageProps } from "$fresh/server.ts";
 import { Playlist } from "#/playlist/playlist.tsx";
 import { readPlaylistFile } from "@/beatsaber-playlist/mod.ts";
 
-const relativePlaylistPath = "../../migrated/playlists";
+const relativePlaylistPath = "../../../../migrated/playlists";
 
 export default function PlaylistItem(props: PageProps) {
   const playlists = [...Deno.readDirSync(relativePlaylistPath)].map(x => x.name);
