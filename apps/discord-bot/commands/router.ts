f9c8b681f1ccf3460b4f2d0a4eea23c386a049c6
Daniel Mizerski <danielduel-github@dduel.dev>
2023-12-19 14:49:44 +0100

    refactor commands - 4

--- apps/discord-bot/commands/router.ts
+++ apps/discord-bot/commands/router.ts
@@ -1,9 +1,12 @@
-
 import { executePing } from "@/apps/discord-bot/commands/commands/ping.ts";
 import { CommandEmptyInteraction } from "@/apps/discord-bot/commands/types.ts";
 import { executeCreateChannelPlaylist } from "@/apps/discord-bot/commands/commands/createChannelPlaylist.ts";
 import { executePlaylists } from "@/apps/discord-bot/commands/commands/playlists.ts";
-import { AdminCommandRoutingGet, AdminCommandRoutingMark } from "@/apps/discord-bot/commands/definitions.ts";
+import {
+  AdminCommandRoutingGet,
+  AdminCommandRoutingMark,
+} from "@/apps/discord-bot/commands/definitions.ts";
+import { respondWithMessage } from "@/apps/discord-bot/commands/utils.ts";
 
 type CommandMapping = {
   "ping": CommandEmptyInteraction;
@@ -25,7 +28,14 @@ export async function router(commandEvent: unknown) {
     return executePing();
   }
 
-  return;
+  if (isCommandValid(commandEvent)) {
+    console.log(commandEvent.data.name);
+    console.log(commandEvent.data.options);
+    return respondWithMessage("404 Command not found", true);
+  } else {
+    console.log(commandEvent);
+    return respondWithMessage("400 Command invalid", true);
+  }
 }
 
 function isCommandOfType<T extends keyof CommandMapping>(
@@ -40,5 +50,18 @@ function isCommandOfType<T extends keyof CommandMapping>(
     typeof commandEvent.data === "object" &&
     "name" in commandEvent.data && typeof commandEvent.data.name === "string" &&
     commandEvent.data.name === nameOfCommand
+  );
+}
+
+function isCommandValid<T extends keyof CommandMapping>(
+  commandEvent: unknown
+): commandEvent is CommandEmptyInteraction {
+  return (
+    typeof commandEvent === "object" &&
+    !!commandEvent &&
+    "data" in commandEvent &&
+    !!commandEvent.data &&
+    typeof commandEvent.data === "object" &&
+    "name" in commandEvent.data && typeof commandEvent.data.name === "string"
   );
 }
