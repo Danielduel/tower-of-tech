554676f27a6c283c6ab73c914f8d617c452a9827
Daniel Mizerski <danielduel-github@dduel.dev>
2023-12-21 02:26:42 +0100

    improve cache cycle 3

--- packages/api-beatsaver/cache.ts
+++ packages/api-beatsaver/cache.ts
@@ -16,7 +16,7 @@ import {
 import { z } from "zod";
 import { buckets } from "@/packages/database-editor/buckets.ts";
 import { filterNulls } from "@/packages/utils/filter.ts";
-import { fetchHashes } from "@/packages/api-beatsaver/mod.ts";
+import { BeatSaverApi } from "@/packages/api-beatsaver/mod.ts";
 
 const watcherName = "api-beatsaver-cache-worker";
 
@@ -31,7 +31,7 @@ export const scheduleCache = async (hashes: LowercaseMapHash[]) => {
 
   let delayS = 1;
   while (remainingHashes.length > 0) {
-    const items = remainingHashes.splice(0, 2);
+    const items = remainingHashes.splice(0, 1);
     await kv.enqueue({
       for: watcherName,
       body: items,
@@ -63,18 +63,15 @@ export const runWorker = () => {
       
       console.log(`Caching ${filteredHashes.length} items to ${buckets.beatSaver.mapByHash}`);
 
-      const response = await fetchHashes(filteredHashes);
+      const response = await BeatSaverApi.mapByHash.get({ urlParams: { hash: filteredHashes[0] }});
 
       if (!response) return;
 
-      const cached = await Promise.all(Object.entries(response)
-        .map(([lowercaseHash, data]) =>
-          s3clientEditor.putObject(lowercaseHash, JSON.stringify(data), {
-            bucketName: buckets.beatSaver.mapByHash,
-          })
-        ));
+      await s3clientEditor.putObject(filteredHashes[0], JSON.stringify(response), {
+        bucketName: buckets.beatSaver.mapByHash,
+      })
       
-      console.log(`Cached ${cached.length} items to ${buckets.beatSaver.mapByHash}`);
+      console.log(`Cached ${filteredHashes[0]} items to ${buckets.beatSaver.mapByHash}`);
     } catch (_) {
       // expected
     }
