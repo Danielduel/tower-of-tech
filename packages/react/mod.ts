5cbe148603dd49df6ee414011ca85c95d8527093
Daniel Mizerski <danielduel-github@dduel.dev>
2023-12-25 19:30:22 +0100

    test reexported react 3

--- packages/react/mod.ts
+++ packages/react/mod.ts
@@ -1,3 +1,3 @@
 // @deno-types="npm:@types/react"
 export { StrictMode, Suspense, forwardRef, useMemo, useState, useRef, useEffect, createContext, createElement, createFactory, createRef } from "https://esm.sh/react@18.2.0?dev";
-export type { FC, PropsWithChildren, Ref, PropsWithRef } from "https://esm.sh/react@18.2.0?dev";
+export type { FC, PropsWithChildren, Ref, PropsWithRef, Fragment } from "https://esm.sh/react@18.2.0?dev";
