3cf5ae269b19539bbe601c2f746064c581cb5b9f
Daniel Mizerski <danielduel-github@dduel.dev>
2024-03-07 23:50:14 +0100

    remove unused scripts

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,17 @@
 import $ from "https://deno.land/x/dax@0.26.0/mod.ts";
 
-const options = [
-  "Update playlists",
-  "Migrate playlists",
-  "Update and migrate playlists",
-  "Generate cup of a day playlist",
-] as const;
-const index = await $.select({
-  message: "What to do?",
-  options: options as unknown as string []
-});
+// const options = [
+//   "Migrate",
+// ] as const;
+// const index = await $.select({
+//   message: "What to do?",
+//   options: options as unknown as string[],
+// });
 
-switch (options[index]) {
-  case "Update playlists":
-    await $`deno task refreshPlaylists`;
-    break;
-  case "Migrate playlists":
-    await $`deno task migratePlaylists`;
-    break;
-  case 'Update and migrate playlists':
-    await $`deno task refreshPlaylists`;
-    await $`deno task migratePlaylists`;
-    break;
-  case "Generate cup of a day playlist":
-    await $`deno task generateCupOfADayPlaylist`;
-    break;
-}
+// switch (options[index]) {
+//   case "Migrate":
+//     await $`deno task migrate`;
+//     break;
+// }
+
+await $`deno task migrate`;
---
+++ src/tools/migrate.ts
@@ -0,0 +1,1 @@
+import "@/src/tools/migratePlaylists.ts";
