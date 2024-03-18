2cf0fab345ae58b3e2875614d831060908bdcf45
Daniel Mizerski <danielduel-github@dduel.dev>
2024-03-18 23:47:10 +0100

    update latest release url

--- packages/utils/constants.ts
+++ packages/utils/constants.ts
@@ -1,3 +1,3 @@
 export const latestPlaylistReleaseUrl =
-  "https://github.com/Danielduel/tower-of-tech/releases/download/0.0.12/ToT.zip";
+  "https://github.com/Danielduel/tower-of-tech/releases/download/0.0.13/ToT.zip";
 export const towerOfTechWebsiteOrigin = "https://tower-of-tech-editor.deno.dev";
