be5e52a4cbb5e94ce1eaaec0c2e8904b4208751b
Daniel Mizerski <danielduel-github@dduel.dev>
2024-03-28 13:28:31 +0100

    chore: cleanup denoflare

--- push.ts
+++
@@ -1,9 +0,0 @@
-import "https://deno.land/std@0.206.0/dotenv/load.ts";
-
-const run = Deno.run({
-  cmd: ["denoflare", "push", "websocket-proxy-beatleader"],
-  stdout: "piped"
-});
-
-const out = await run.status();
-console.log(out);
