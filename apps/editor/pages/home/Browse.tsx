d9934b146e182e445eb781e51971bc0a7746fbca
Daniel Mizerski <danielduel-github@dduel.dev>
2024-06-03 02:48:53 +0200

    fix: retext

--- apps/editor/pages/home/Browse.tsx
+++ apps/editor/pages/home/Browse.tsx
@@ -38,27 +38,35 @@ export const Browse = forwardRef<HTMLDivElement>(
           <i className="text-[#a3be8c] font-bold highlight">&gt;8</i>,{" "}
           <i className="text-[#ebcb8b] font-bold highlight">&gt;10</i>,{" "}
           <i className="text-[#d08770] font-bold highlight">10+</i> nps.<br />
-          I recommend&nbsp;
           <i className="text-[#5e81ac] font-bold highlight">Adep</i>{" "}
-          for beginners and and people working on fundamentals.<br />While upper
-          boundary of{" "}
+          is great for beginners and people who work on fundamentals.<br />
           <i className="text-[#88c0d0] font-bold highlight">
             Acc
           </i>{" "}
-          can be already perceived as midspeedy.
+          gives a slight challenge for accing, good for semiadvanced players.
           <br />
           <br />
           Second segment:<br />
-          <i>Comfy</i>, <i>Tech</i>, <i>Hitech</i>, <i>Anglehell</i>,{" "}
-          <i>Tempo</i> are complexities.<br />
+          <i className="font-bold highlight">Comfy</i>,{" "}
+          <i className="font-bold highlight">Tech</i>,{" "}
+          <i className="font-bold highlight">Hitech</i>,{" "}
+          <i className="font-bold highlight">Anglehell</i>,{" "}
+          <i className="font-bold highlight">Tempo</i> are complexities.<br />
           If you are new to BeatSaber or you are a linear player who want to
-          practice tech - I recommend starting with <i>Comfy</i>.<br />
-          <i>Tech</i> is a bit more complex than{" "}
-          <i>Comfy</i>, great for semiadvanced tech crowd.<br />
-          <i>Hitech</i> is much more complex than{" "}
-          <i>Tech</i>, targets superadvanced tech players.<br />
-          <i>Anglehell</i> and <i>Tempo</i> are especially hard cases of{" "}
-          <i>Hitech</i>.
+          practice tech - I recommend starting with{" "}
+          <i className="font-bold highlight">Comfy</i>.<br />
+          <i className="font-bold highlight">Tech</i> is a bit more complex than
+          {" "}
+          <i className="font-bold highlight">Comfy</i>, great for semiadvanced
+          tech crowd.<br />
+          <i className="font-bold highlight">Hitech</i>{" "}
+          is much more complex than{" "}
+          <i className="font-bold highlight">Tech</i>, targets superadvanced
+          tech players.<br />
+          <i className="font-bold highlight">Anglehell</i> and{" "}
+          <i className="font-bold highlight">Tempo</i>{" "}
+          are especially hard cases of{" "}
+          <i className="font-bold highlight">Hitech</i>.
         </VisualNovelStepExplanation>
         <VisualNovelDivider />
         <VisualNovelStepInlineATag
