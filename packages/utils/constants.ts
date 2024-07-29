06e0e01c75ccb9e1ba8433a8f0ce7715143e58ae
Daniel Mizerski <danielduel-github@dduel.dev>
2024-07-29 18:36:04 +0200

    tech: point to new version

--- packages/utils/constants.ts
+++ packages/utils/constants.ts
@@ -1,3 +1,3 @@
 export const latestPlaylistReleaseUrl =
-  "https://github.com/Danielduel/tower-of-tech/releases/download/0.0.16/ToT.zip";
+  "https://github.com/Danielduel/tower-of-tech/releases/download/0.0.17/ToT.zip";
 export const towerOfTechWebsiteOrigin = "https://towerofte.ch";
