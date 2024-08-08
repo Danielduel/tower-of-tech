860830bb1e62307cd927b2853ead7cea30d99ed6
Daniel Mizerski <danielduel-github@dduel.dev>
2024-08-08 18:54:29 +0200

    fix: upgrade recorder to autoreconnect to BL

--- apps/beatleader-bot/tools/record.ts
+++ apps/beatleader-bot/tools/record.ts
@@ -1,59 +1,86 @@
 import { createBeatleaderWSGeneral } from "@/apps/beatleader-bot/sockets/beatleader-socket-general.ts";
-
-const ws = createBeatleaderWSGeneral();
+import { Err } from "@/apps/beatleader-bot/deps.ts";
 
 const historyDir = `./data/beatleader-wss-history/${Date.now()}`;
-let autoInc = 0;
+let autoIncHistory = 0;
+let autoIncLog = 0;
+let socketNum = 0;
 
 const writeHistory = (
   ...data: unknown[]
 ) => {
-  const path = `${historyDir}/${Date.now()}-${++autoInc}`;
+  const path = `${historyDir}/${Date.now()}-${socketNum}-${++autoIncHistory}`;
+  return Deno.writeTextFile(path, JSON.stringify(data));
+};
+
+const writeLog = (
+  ...data: unknown[]
+) => {
+  const path = `${historyDir}/${Date.now()}-${socketNum}-${++autoIncLog}`;
   return Deno.writeTextFile(path, JSON.stringify(data));
 };
 
 Deno.mkdirSync(historyDir, { recursive: true });
 
-ws.on("accepted", async (...data: unknown[]) => {
-  console.log("accepted");
-  await writeHistory("accepted", data);
-});
+const createBeatleaderWSGeneralRecorder = () =>
+  new Promise((resolve) => {
+    const ws = createBeatleaderWSGeneral();
 
-ws.on("accepted_errorParse", async (...data: unknown[]) => {
-  console.log("accepted_errorParse");
-  await writeHistory("accepted_errorParse", data);
-  Deno.exit();
-});
+    ws.on("accepted", async (...data: unknown[]) => {
+      console.log("accepted");
+      await writeHistory("accepted", data);
+    });
 
-ws.on("close", async (...data: unknown[]) => {
-  console.log("close");
-  await writeHistory("close", data);
-  Deno.exit();
-});
+    ws.on("accepted_errorParse", async (...data: unknown[]) => {
+      console.log("accepted_errorParse");
+      await writeHistory("accepted_errorParse", data);
+      resolve(Err("accepted_errorParse"));
+    });
 
-ws.on("error", async (...data: unknown[]) => {
-  console.log("error");
-  await writeHistory("error", data);
-  Deno.exit();
-});
+    ws.on("close", async (...data: unknown[]) => {
+      console.log("close");
+      await writeHistory("close", data);
+      resolve(Err("Close"));
+    });
 
-ws.on("message_errorParse", async (...data: unknown[]) => {
-  console.log("message_errorParse");
-  await writeHistory("message_errorParse", data);
-  Deno.exit();
-});
+    ws.on("error", async (...data: unknown[]) => {
+      console.log("error");
+      await writeHistory("error", data);
+      resolve(Err("Error"));
+    });
 
-ws.on("open", async (...data: unknown[]) => {
-  console.log("open");
-  await writeHistory("open", data);
-});
+    ws.on("message_errorParse", async (...data: unknown[]) => {
+      console.log("message_errorParse");
+      await writeHistory("message_errorParse", data);
+      resolve(Err("message_errorParse"));
+    });
 
-ws.on("rejected", async (...data: unknown[]) => {
-  console.log("upload");
-  await writeHistory("upload", data);
-});
+    ws.on("open", async (...data: unknown[]) => {
+      console.log("open");
+      await writeHistory("open", data);
+    });
 
-ws.on("upload", async (...data: unknown[]) => {
-  console.log("upload");
-  await writeHistory("upload", data);
-});
+    ws.on("rejected", async (...data: unknown[]) => {
+      console.log("upload");
+      await writeHistory("upload", data);
+    });
+
+    ws.on("upload", async (...data: unknown[]) => {
+      console.log("upload");
+      await writeHistory("upload", data);
+    });
+  });
+
+// TODO - not sure if restarting it in this way will not leave out polluted scope of the dangling promise/websocket
+while (true) {
+  try {
+    const exitResolved = await createBeatleaderWSGeneralRecorder();
+    console.log("Connection reset - resolve");
+    writeLog("connection_reset_resolve", exitResolved);
+    socketNum++;
+  } catch (err) {
+    console.log("Connection reset - error");
+    writeLog("connection_reset_error", err);
+    socketNum++;
+  }
+}
