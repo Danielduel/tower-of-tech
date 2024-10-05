e3825a762a3ca47b536d3227e2ea7c131ce22eaa
Daniel Mizerski <danielduel-github@dduel.dev>
2024-10-06 01:21:57 +0200

    prepare release

--- packages/utils/constants.ts
+++ packages/utils/constants.ts
@@ -1,2 +1,2 @@
-export const latestPlaylistReleaseUrl = "https://github.com/Danielduel/tower-of-tech/releases/download/0.0.18/ToT.zip";
+export const latestPlaylistReleaseUrl = "https://github.com/Danielduel/tower-of-tech/releases/download/0.0.19/ToT.zip";
 export const towerOfTechWebsiteOrigin = "https://towerofte.ch";
