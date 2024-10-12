25b24fcda4b4f96b09497d2c299c70a170eeda35
Daniel Mizerski <danielduel-github@dduel.dev>
2024-10-13 01:40:09 +0200

    fix: imports for auth-api e2e

--- packages/api-twitch/auth-api.e2e.ts
+++ packages/api-twitch/auth-api.e2e.ts
@@ -1,7 +1,7 @@
 import { defaultHeaders, TwitchAuthApi } from "@/packages/api-twitch/auth-api.ts";
-import { getChannelDataByBroadcasterName, setChannelData } from "@/packages/api-twitch/helix-api.ts";
+import { getChannelDataByBroadcasterName, setChannelData } from "@/packages/api-twitch/helix/TwitchHelixApiClient.ts";
 
-import { getUserToken } from "./auth-api-get-user-access-token.e2e.ts";
+import { getUserToken } from "@/packages/api-twitch/auth-api-get-user-access-token.e2e.ts";
 import { createAppAuthorizationHeaders, createUserAuthorizationHeaders } from "@/packages/api-twitch/helix/common.ts";
 
 const userCredentials = await getUserToken();
@@ -10,7 +10,6 @@ const client_id = Deno.env.get("TWITCH_API_CLIENT_ID")!;
 const client_secret = Deno.env.get("TWITCH_API_CLIENT_SECRET")!;
 const grant_type = Deno.env.get("TWITCH_API_GRANT_TYPE")!;
 const masterChannel = Deno.env.get("TWITCH_API_MASTER_CHANNEL")!;
-const masterChannelId = Deno.env.get("TWITCH_API_MASTER_CHANNEL_ID")!;
 
 const urlParams = new URLSearchParams({
   client_id,
@@ -43,5 +42,3 @@ const patchChannelResponse = await setChannelData(
 );
 
 console.log(patchChannelResponse);
-
-// console.log(schedule.scheduledStartTime, schedule.scheduledEndTime);
