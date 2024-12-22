b505937d42f821dffa3273a1ad02b797ea04ea23
Daniel Mizerski <danielduel-github@dduel.dev>
2024-12-23 00:33:32 +0100

    fix: readable 'more' page

--- apps/website/pages/home/Browse.tsx
+++ apps/website/pages/home/Browse.tsx
@@ -14,42 +14,44 @@ export const Browse = forwardRef<HTMLDivElement>(
       <VisualNovelStep ref={ref}>
         <VisualNovelStepExplanation>
           First segment:<br />
-          <i className="text-[#5e81ac] font-bold highlight">Adep</i>,&nbsp;
-          <i className="text-[#88c0d0] font-bold highlight">
+          <i className="text-[#5e81ac] font-bold highlight pr-1">Adep</i>,&nbsp;
+          <i className="text-[#88c0d0] font-bold highlight pr-1">
             Acc
-          </i>,&nbsp;<i className="text-[#a3be8c] font-bold highlight">
+          </i>,&nbsp;<i className="text-[#a3be8c] font-bold highlight pr-1">
             Mid
-          </i>,&nbsp;<i className="text-[#ebcb8b] font-bold highlight">
+          </i>,&nbsp;<i className="text-[#ebcb8b] font-bold highlight pr-1">
             Fas
-          </i>,&nbsp;<i className="text-[#d08770] font-bold highlight">
+          </i>,&nbsp;<i className="text-[#d08770] font-bold highlight pr-1">
             Sonic
           </i>&nbsp;are pacings - "speedyness".<br />
-          Those roughly translate to <i className="text-[#5e81ac] font-bold highlight">&gt;4</i>,{" "}
-          <i className="text-[#88c0d0] font-bold highlight">&gt;6</i>,{" "}
-          <i className="text-[#a3be8c] font-bold highlight">&gt;8</i>,{" "}
-          <i className="text-[#ebcb8b] font-bold highlight">&gt;10</i>,{" "}
-          <i className="text-[#d08770] font-bold highlight">10+</i> nps.<br />
-          <i className="text-[#5e81ac] font-bold highlight">Adep</i>{" "}
+          Those roughly translate to <i className="text-[#5e81ac] font-bold highlight pr-1">&gt;4</i>,{" "}
+          <i className="text-[#88c0d0] font-bold highlight pr-1">&gt;6</i>,{" "}
+          <i className="text-[#a3be8c] font-bold highlight pr-1">&gt;8</i>,{" "}
+          <i className="text-[#ebcb8b] font-bold highlight pr-1">&gt;10</i>,{" "}
+          <i className="text-[#d08770] font-bold highlight pr-1">10+</i> nps.<br />
+          <i className="text-[#5e81ac] font-bold highlight pr-1">Adep</i>{" "}
           is great for beginners and people who work on fundamentals.<br />
-          <i className="text-[#88c0d0] font-bold highlight">
+          <i className="text-[#88c0d0] font-bold highlight pr-1">
             Acc
           </i>{" "}
           gives a slight challenge for accing, good for semiadvanced players.
           <br />
           <br />
           Second segment:<br />
-          <i className="font-bold highlight">Comfy</i>, <i className="font-bold highlight">Tech</i>,{" "}
-          <i className="font-bold highlight">Hitech</i>, <i className="font-bold highlight">Anglehell</i>,{" "}
-          <i className="font-bold highlight">Tempo</i> are complexities.<br />
+          <i className="font-bold highlight pr-1">Comfy</i>, <i className="font-bold highlight pr-1">Tech</i>,{" "}
+          <i className="font-bold highlight pr-1">Hitech</i>, <i className="font-bold highlight pr-1">Anglehell</i>,
+          {" "}
+          <i className="font-bold highlight pr-1">Tempo</i> are complexities.<br />
           If you are new to BeatSaber or you are a linear player who want to practice tech - I recommend starting with
           {" "}
-          <i className="font-bold highlight">Comfy</i>.<br />
-          <i className="font-bold highlight">Tech</i> is a bit more complex than{" "}
-          <i className="font-bold highlight">Comfy</i>, great for semiadvanced tech crowd.<br />
-          <i className="font-bold highlight">Hitech</i> is much more complex than{" "}
-          <i className="font-bold highlight">Tech</i>, targets superadvanced tech players.<br />
-          <i className="font-bold highlight">Anglehell</i> and <i className="font-bold highlight">Tempo</i>{" "}
-          are especially hard cases of <i className="font-bold highlight">Hitech</i>.
+          <i className="font-bold highlight pr-1">Comfy</i>.<br />
+          <i className="font-bold highlight pr-1">Tech</i> is a bit more complex than{" "}
+          <i className="font-bold highlight pr-1">Comfy</i>, great for semiadvanced tech crowd.<br />
+          <i className="font-bold highlight pr-1">Hitech</i> is much more complex than{" "}
+          <i className="font-bold highlight pr-1">Tech</i>, targets superadvanced tech players.<br />
+          <i className="font-bold highlight pr-1">Anglehell</i> and <i className="font-bold highlight pr-1">Tempo</i>
+          {" "}
+          are especially hard cases of <i className="font-bold highlight pr-1">Hitech</i>.
         </VisualNovelStepExplanation>
         <VisualNovelDivider />
         <VisualNovelStepInlineATag
--- apps/website/pages/home/More.tsx
+++ apps/website/pages/home/More.tsx
@@ -13,49 +13,68 @@ export const More = forwardRef<HTMLDivElement>((_, ref) => {
       <hr />
       <VisualNovelDivider />
       The speed part:<br />
-      Adep, Acc, Mid, Fas, Sonic - are based on burstable swings per second, which roughly translated to nps would
-      be...<br />
-      <br />
-      4nps for Adep<br />
-      4-6nps for Acc<br />
-      6-8nps for Mid<br />
-      8-11nps for Fas<br />
-      11+nps for Sonic<br />
+      <VisualNovelStepExplanation>
+        <i className="text-[#5e81ac] font-bold highlight pr-1">Adep</i>,&nbsp;
+        <i className="text-[#88c0d0] font-bold highlight pr-1">
+          Acc
+        </i>,&nbsp;<i className="text-[#a3be8c] font-bold highlight pr-1">
+          Mid
+        </i>,&nbsp;<i className="text-[#ebcb8b] font-bold highlight pr-1">
+          Fas
+        </i>,&nbsp;<i className="text-[#d08770] font-bold highlight pr-1">
+          Sonic
+        </i>{" "}
+        sort pacings, the "speedyness"<br />
+        <br />
+        <i className="text-[#5e81ac] font-bold highlight pr-1">Adep</i> is less than 4nps<br className="mb-1" />
+        <i className="text-[#88c0d0] font-bold highlight pr-1">Acc</i> is less than 6nps<br className="m-1" />
+        <i className="text-[#a3be8c] font-bold highlight pr-1">Mid</i> is less than 8nps<br className="m-1" />
+        <i className="text-[#ebcb8b] font-bold highlight pr-1">Fas</i> is less than 10-11nps<br className="m-1" />
+        <i className="text-[#d08770] font-bold highlight pr-1">Sonic</i> is anything more than Fas<br className="m-1" />
+        <VisualNovelStepExplanation>
+          With few details:<br />
+          Bursts/jumps/empty spaces move a map to faster category.<br />
+          Sliders/dotspam move a map to slower category.<br />
+        </VisualNovelStepExplanation>
+      </VisualNovelStepExplanation>
+
       <VisualNovelDivider />
       <hr />
       <VisualNovelDivider />
       The complexity part:<br />
-      Comfy, Tech, Hitech, Anglehell, Tempo are more subjective:<br />
-      <br />
-      <i>Comfy</i>{" "}
-      consists mostly of simple/comfy moves, but it has sections which players new to tech would find harder than usual.
       <VisualNovelStepExplanation>
-        Speed players can reduce their amount of what they call "shitmisses" by playing MidComfy/FasComfy playlists.
-      </VisualNovelStepExplanation>
-      <br />
-      <i>Tech</i>{" "}
-      introduces a lot of sidecuts, returns, bottomstreams, inlines, nonlinear jumps, ocassional palmups and rolls that
-      aren't essential to the flow.<br />
-      <br />
-      <i>Hitech</i>{" "}
-      constists of moves that tech had, but it puts them into much longer and harder to read combinations.<br />
-      <br />
-      <i>Anglehell</i>{" "}
-      constantly rotates blocks, it is heavier form of Hitech in the way that you have to pay attention to things like
-      an ocassional sidecut inside a bottomstream or koji patterns.<br />
-      <br />
-      <i>Tempo</i>{" "}
-      is not necessarily harder than Anglehell, it is easier to fullcombo if you absolutely feel the flow, but one
-      mistake often leads to a quick fail.
-      <VisualNovelStepExplanation>
-        As Anglehell was about making each block hard, Tempo is more about navigating in a giant web of possible
-        patterns.
+        <i className="font-bold highlight pr-1">Comfy</i>, <i className="font-bold highlight pr-1">Tech</i>,{" "}
+        <i className="font-bold highlight pr-1">Hitech</i>, <i className="font-bold highlight pr-1">Anglehell</i>,{" "}
+        <i className="font-bold highlight pr-1">Tempo</i> describe complexity, the "techyness"<br />
+        <br />
+        <i className="font-bold highlight pr-1">Comfy</i>{" "}
+        consists mostly of simple/comfy moves, but it has sections which players new to tech would find harder than
+        usual.
+        <VisualNovelStepExplanation>
+          Speed players can reduce their amount of what they call "shitmisses" by playing MidComfy/FasComfy playlists.
+        </VisualNovelStepExplanation>
+        <br />
+        <i className="font-bold highlight pr-1">Tech</i>{" "}
+        introduces a lot of sidecuts, returns, bottomstreams, inlines, nonlinear jumps, ocassional palmups and rolls
+        that aren't essential to the flow.<br />
+        <br />
+        <i className="font-bold highlight pr-1">Hitech</i>{" "}
+        constists of moves that tech had, but it puts them into much longer and harder to read combinations.<br />
+        <br />
+        <i className="font-bold highlight pr-1">Anglehell</i>{" "}
+        constantly rotates blocks, it is heavier form of Hitech in the way that you have to pay attention to things like
+        an ocassional sidecut inside a bottomstream or koji patterns.<br />
+        <br />
+        <i className="font-bold highlight pr-1">Tempo</i>{" "}
+        is not necessarily harder than Anglehell, it is easier to fullcombo if you absolutely feel the flow, but one
+        mistake often leads to a quick fail.
+        <VisualNovelStepExplanation>
+          As Anglehell was about making each block hard, Tempo is more about navigating in a giant web of possible
+          patterns.
+        </VisualNovelStepExplanation>
       </VisualNovelStepExplanation>
       <VisualNovelDivider />
       <hr />
-      <VisualNovelDivider />
-      Now, since this naming convention is often confusing, there are talks about how to rename those and to rework the
-      gradation.
       <VisualNovelDivider />
       <VisualNovelStepLink
         to={links.home.history}
