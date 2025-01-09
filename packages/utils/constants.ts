1a3790964b8aff84d6645b696247d1d6c0dfe77a
Daniel Mizerski <danielduel-github@dduel.dev>
2025-01-09 16:38:51 +0100

    tech: update pool, push version

--- packages/utils/constants.ts
+++ packages/utils/constants.ts
@@ -1,2 +1,2 @@
-export const latestPlaylistReleaseUrl = "https://github.com/Danielduel/tower-of-tech/releases/download/0.0.22/ToT.zip";
+export const latestPlaylistReleaseUrl = "https://github.com/Danielduel/tower-of-tech/releases/download/0.0.23/ToT.zip";
 export const towerOfTechWebsiteOrigin = "https://towerofte.ch";
