583936ebea8f8cd83cc9be532165c4e7c12013ff
Daniel Mizerski <danielduel-github@dduel.dev>
2025-09-01 07:39:24 +0200

    fix: change redirect method

--- apps/website/routes/index.tsx
+++ apps/website/routes/index.tsx
@@ -2,12 +2,6 @@ import { Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers = {
   GET(_req, _ctx) {
-    const headers = new Headers({
-      location: "/home",
-    });
-    return new Response(null, {
-      status: 302,
-      headers,
-    });
+    return Response.redirect("/home", 307)
   },
 };
