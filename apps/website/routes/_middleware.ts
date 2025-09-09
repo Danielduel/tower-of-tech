171e6a730b748e2fb9951d2a2a54e8f1b0dd6752
Daniel Mizerski <danielduel-github@dduel.dev>
2025-09-09 17:31:23 +0200

    fix: reduce bot traffic

--- apps/website/routes/_middleware.ts
+++ apps/website/routes/_middleware.ts
@@ -1,10 +1,21 @@
 import { FreshContext } from "$fresh/server.ts";
 
+// basic list to reduce bot traffic
+const endsWithBlock = [
+  ".php",
+  ".php7"
+];
+
+
 export async function handler(
   req: Request,
   ctx: FreshContext,
 ) {
+  if (endsWithBlock.some(ending => req.url.split("?")[0].endsWith(ending))) {
+    return new Response("https://www.youtube.com/watch?v=E0Ok6HW9-Fw", { status: 307, headers: { location: "https://www.youtube.com/watch?v=E0Ok6HW9-Fw" }});
+  }
   console.log(`Access ${req.url}`)
   const resp = await ctx.next();
   return resp;
 }
+
